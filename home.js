e0b7ca8ea846d3f2ea40d58e2274ee6376cd4e3e
MarleyHU19 <32494085+MarleyHU19@users.noreply.github.com>
2017-10-17 11:10:10 -0400

    Add files via upload

---
+++ home.js
@@ -0,0 +1,30 @@
+window.addEventListener("keyup", switchTabs, false);  
+    function switchTabs(key) {
+        var nextTab = document.getElementById("topnav")
+    if (key.keyCode == "37"){
+        switch(nextTab)
+            case Home:
+            
+               
+        location.href="friends_pg5.html"
+    }
+    if (key.keyCode == "38"){
+       location.href="friends_pg5.html"
+    }
+    if (key.keyCode == "39"){
+        
+        location.href="courses_pg2.html"
+    }
+    if (key.keyCode == "40"){
+        
+        location.href="courses_pg2.html"
+    }
+}
+
+function scrollFunction() {
+    
+    (onscroll.mousedown
+    location.href="courses_pg2.html"
+}
+
+window.onscroll = scrollFunction;
