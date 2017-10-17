aac00f172eba503c3e938777d3cbc7169465df9a
MarleyHU19 <32494085+MarleyHU19@users.noreply.github.com>
2017-10-17 11:30:37 -0400

    Add files via upload

--- tabRotator.js
+++ tabRotator.js
@@ -2,17 +2,16 @@ var secondsClock = 0;
 document.onclick = function() {
     secondsClock = 0;};
 
-document.onmousemove = function() {
-    secondsClock = 0;};
-
 document.onkeypress = function() {
     secondsClock = 0;};
 
-window.setInterval(tabRotator, 1000);
+document.onmousemove = function() {
+    secondsClock = 0;};
 
 function tabRotator() {
     secondsClock++;
     if (secondsClock == 30) {
-        location.href="courses_link.html"
+        location.href="courses_pg2.html"
     }
 }
+window.setInterval(tabRotator, 1000); //checks event every one secon 
